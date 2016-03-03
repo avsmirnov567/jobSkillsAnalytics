75445eb2fec7473ecd8d10cef850b9b81a158b5b
Алексей Мальцев <maltsevaa@proitr.ru>
2016-03-03 13:21:27 +0500

    Added CSV-exporting frature

---
+++ Parser/CsvVacancyExporter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+
+namespace Parser
+{
+    class CsvVacancyExporter
+    {
+        private List<Vacancy> vacancies;
+
+        public CsvVacancyExporter(List<Vacancy> vacancies)
+        {
+            this.vacancies = vacancies;
+        }
+
+        public void ExportVacancies(string fileName = "")
+        {
+            string name = String.IsNullOrEmpty(fileName) ? "vacancies_" + Guid.NewGuid().ToString() + ".csv" : fileName;
+            using (StreamWriter writer = new StreamWriter(name))
+            {
+                var csvWriter = new CsvWriter(writer);
+                csvWriter.Configuration.RegisterClassMap<VacancyCsvMapping>();
+                csvWriter.WriteRecords(vacancies);
+            }
+        }
+
+        public void ExportSkills(string fileName = "")
+        {
+            string name = String.IsNullOrEmpty(fileName) ? "skills_" + Guid.NewGuid().ToString() + ".csv" : fileName;
+            using (StreamWriter writer = new StreamWriter(name))
+            {
+                var csvWriter = new CsvWriter(writer);
+                csvWriter.WriteField("vacancy_id");
+                csvWriter.WriteField("vacancy_link");
+                csvWriter.WriteField("skill");                
+                foreach (var vacancy in vacancies)
+                {                    
+                    foreach (var skill in vacancy.Skills)
+                    {
+                        csvWriter.NextRecord();
+                        csvWriter.WriteField(vacancy.IDfromSite);
+                        csvWriter.WriteField(vacancy.Link);
+                        csvWriter.WriteField(skill);
+                    }
+                }
+            }
+        }
+    }
+}

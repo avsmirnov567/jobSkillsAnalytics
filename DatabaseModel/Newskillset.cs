6c88d02239c0726e5dd02b413060094c6190cecc
Aleksandr Smirnov <avsmirnov567@gmail.com>
2016-03-03 22:13:25 +0500

    Add data model for database

---
+++ DatabaseModel/Newskillset.cs
@@ -0,0 +1,28 @@
+namespace DatabaseModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Newskillset")]
+    public partial class Newskillset
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Newskillset()
+        {
+            Skills = new HashSet<Skills>();
+        }
+
+        [Key]
+        public int newskillset_id { get; set; }
+
+        public int skillset_number { get; set; }
+
+        public decimal? skillset_support { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Skills> Skills { get; set; }
+    }
+}

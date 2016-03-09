96d24cce544cf98f546cc85165a8edae0dec2663
Alexey Maltsev <axel.user.perm@gmail.com>
2016-03-09 23:04:55 +0500

    Troubleshooting

---
+++ Portal/Controllers/TextMiningController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JobSkillsDb.Entities;
+
+namespace Portal.Controllers
+{
+    public class TextMiningController : Controller
+    {
+        // GET: TextMining
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id = 0)
+        {
+            using(JobSkillsContext db = new JobSkillsContext())
+            {
+                Vacancy vacancy = id > 0 ? db.Vacancies.Find(id) : db.Vacancies.First();
+                return View(vacancy);
+            }
+        }
+
+    }
+}
--- Portal/Properties/AssemblyInfo.cs
+++ Portal/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9354de32-6a0b-43cb-8217-8476ac058860")]
+[assembly: Guid("9282ef82-a1ac-4bd9-9623-33e762479987")]
 
 // Version information for an assembly consists of the following four values:
 //

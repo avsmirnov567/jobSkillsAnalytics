e4793b1c65be51fb4658a6dff3d35aeb6188ada3
Alexey Maltsev <axel.user.perm@gmail.com>
2016-03-19 11:03:07 +0500

    Добавил контроллер для статистики

---
+++ Portal/Controllers/StatController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JobSkillsDb.Entities;
+
+namespace Portal.Controllers
+{
+    public class StatController : Controller
+    {
+        // GET: Stat
+        public ActionResult Index()
+        {
+            using(JobSkillsContext db = new JobSkillsContext())
+            {
+                ViewBag.VacanciesCount = db.Vacancies.Count();
+                ViewBag.SkillsCount = db.Skills.Count();
+            }
+            return View();
+        }
+    }
+}

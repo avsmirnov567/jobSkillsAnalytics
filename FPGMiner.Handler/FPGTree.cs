31b3be42588413345fe884681d3113ad3ebaee9c
Alexey Maltsev <axel.user.perm@gmail.com>
2016-03-20 15:42:06 +0500

    Started implementing F-P Growth algo

---
+++ FPGMiner.Handler/FPGTree.cs
@@ -0,0 +1,22 @@
+﻿using JobSkillsDb.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPGMiner.Handler
+{
+    public class FPGTree
+    {
+        public FPGTreeNode Root { get; set; }
+
+        public void Add(List<Skill> transaction)
+        {
+            FPGTreeNode curNode = Root;
+            //if(curNode.ChildNodes.Contains()
+        }
+
+        //private void Add
+    }
+}

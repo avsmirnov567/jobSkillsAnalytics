52ff406d573fc9aa33b9367041fa2d3a253c2411
Vladislav Rudskoy <rudskoy.vladislav@gmail.com>
2016-03-21 23:00:33 +0500

    Add controllers for apriori rules and eclat frequent sets generation

---
+++ Portal/Controllers/EclatSetController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portal.Controllers
+{
+    public class EclatSetController : Controller
+    {
+        // GET: EclatSet
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

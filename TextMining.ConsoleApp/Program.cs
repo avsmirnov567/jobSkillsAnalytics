85d33499eb087cd9adbaa91210de1889c5ece878
Alexey Maltsev <axel.user.perm@gmail.com>
2016-03-23 03:39:55 +0500

    Added GetFrequentSkills method

--- TextMining.ConsoleApp/Program.cs
+++ TextMining.ConsoleApp/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JobSkillsDb.Entities;
+using System.Data.Entity;
+using TextMining.Handler;
+using System.Text.RegularExpressions;
 
 namespace TextMining.ConsoleApp
 {
@@ -10,6 +14,8 @@ namespace TextMining.ConsoleApp
     {
         static void Main(string[] args)
         {
+            var skills = SkillsAnalytics.GetFrequentSkills(0.01);
+            skills = skills.Where(s => !Regex.IsMatch(s.Name, "[А-я]+")).ToList();
         }
     }
 }

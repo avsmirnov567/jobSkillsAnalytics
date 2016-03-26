2292eb5254f716fb9233a445410b0a605bd602fa
Vladislav Rudskoy <rudskoy.vladislav@gmail.com>
2016-03-27 00:39:46 +0500

    Change recommendation method for multiple skills in input

--- Portal/Controllers/RecommendationsController.cs
+++ Portal/Controllers/RecommendationsController.cs
@@ -15,28 +15,104 @@ namespace Portal.Controllers
             return View();
         }
 
+        //public ActionResult RecomendationList(string name)
+        //{
+        //    using(JobSkillsContext db = new JobSkillsContext())
+        //    {
+        //        List<string> recommendations = db.EclatSets
+        //            .Where(s => s.ItemSet.Contains("," + name + ","))
+        //            .OrderByDescending(s => s.Support)
+        //            .Select(s => s.ItemSet)
+        //            .Take(20)
+        //            .ToList();
+        //        List<string> list = new List<string>();
+        //        foreach(string set in recommendations)
+        //        {
+        //            string[] skills = set.ToLower()
+        //                .Trim(',')
+        //                .Split(',');
+        //            list.AddRange(skills.Except(list));
+        //        }
+        //        list.Remove(name.ToLower());
+        //        list = list.Take(5).ToList();
+        //        return PartialView(list);
+        //    }
+        //}
+
         public ActionResult RecomendationList(string name)
         {
-            using(JobSkillsContext db = new JobSkillsContext())
+
+            //final recomendation to the Console formatting in this method
+
+            var parsedInput = ParseInputForRecommend(name);
+
+            var context = new JobSkillsContext();
+            var dbEclat = context.EclatSets;
+
+            //contains intersections
+            var tupleSet = new List<Tuple<double, string>>();
+            var recomendedTupleSet = new List<Tuple<double, string>>();
+
+            while (parsedInput.Count != 0)
             {
-                List<string> recommendations = db.EclatSets
-                    .Where(s => s.ItemSet.Contains("," + name + ","))
-                    .OrderByDescending(s => s.Support)
-                    .Select(s => s.ItemSet)
-                    .Take(20)
-                    .ToList();
-                List<string> list = new List<string>();
-                foreach(string set in recommendations)
+                foreach (var set in dbEclat)
                 {
-                    string[] skills = set.ToLower()
-                        .Trim(',')
-                        .Split(',');
-                    list.AddRange(skills.Except(list));
+                    var templist = new List<string>();
+                    var parsedSet = ParseInputForRecommend(set.ItemSet);
+
+                    if (parsedSet[parsedSet.Count - 1] == "")
+                    {
+
+                        for (var i = 1; i < parsedSet.Count - 1; i++)
+                        {
+                            templist.Add(parsedSet[i]);
+                        }
+                        parsedSet = templist;
+                    }
+
+                    var checkIntersection = parsedInput.All(s => parsedSet.Contains(s));
+
+                    if (!checkIntersection || parsedSet.Count <= parsedInput.Count) continue;
+                    var support = set.Support;
+
+                    tupleSet.Add(
+                        new Tuple<double, string>(support, set.ItemSet)
+                        );
+
+                    var recommendedItemsTail = parsedSet.Except(parsedInput).ToList();
+                    var recommendedString = string.Join(",", recommendedItemsTail);
+
+                    recomendedTupleSet.Add(
+                        new Tuple<double, string>(support, recommendedString));
                 }
-                list.Remove(name.ToLower());
-                list = list.Take(5).ToList();
-                return PartialView(list);
+                if (parsedInput.Count != 0)
+                    parsedInput.RemoveAt(parsedInput.Count - 1);
+
+                if (recomendedTupleSet.Count > 100) break;
             }
+            var sortedTupleRecomendationList = recomendedTupleSet.OrderBy(i => i.Item1).Take(10).ToList();
+
+            var united = new List<string>();
+
+            foreach (var a in sortedTupleRecomendationList)
+            {
+                var spl = a.Item2.Split(',');
+                united.AddRange(spl);
+            }
+
+            var noDups = united.Distinct().ToList();
+
+            return PartialView(noDups);
+        }
+
+        public static List<string> ParseInputForRecommend(string input)
+        {
+            var parsedInput = input.Split(new char[] { ',' }, StringSplitOptions.None).ToList();
+            for (int i = 0; i < parsedInput.Count; i++)
+            {
+                parsedInput[i] = parsedInput[i].Trim(' ');
+            }
+            return parsedInput;
         }
     }
 }
